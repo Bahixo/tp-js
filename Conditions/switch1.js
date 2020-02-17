99a536fb5cb5b04c434e4881cfa22c89f477540c
Joel.A <bahixo@hotmail.fr>
2020-02-17 11:15:11 +0100

    rajout fichier html pour tp var, tp condition switch 1 terminé, switch 2 en cours

---
+++ Conditions/switch1.js
@@ -0,0 +1,23 @@
+let name = prompt("Spartiate! Quel est votre prénom?!");
+
+switch(name){
+    case "Sarah":
+        alert("Sarah Connor?");
+        break;
+
+    case "Siri":
+        alert("Je n'ai pas compris votre question");
+        break;
+
+    case "Joel":
+        alert("Ce prénom me dit quelque chose..");
+        break;
+
+    case "Vou":
+        alert("Si je suis Vou et qu'il est Moa alors qui êtes vous?");
+        break;
+
+    default:
+        alert("..Au moins vous semblez en bonne santé ! :)");
+        break;
+}
